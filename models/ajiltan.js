0dfd4959350efcf500b459103d28da93580deab6
bilgeedev <jrrsenpai@gmail.com>
2025-10-09 12:03:07 +0800

    z

--- models/ajiltan.js
+++ models/ajiltan.js
@@ -73,7 +73,7 @@ AjiltanModel.estimatedDocumentCount().then((count) => {
         nevtrekhNer: "Admin",
         utas: "Admin",
         mail: "Admin",
-        erkh: "Admin",
+        erkh: "superAdmin",
         register: "Admin",
         albanTushaal: "Admin",
         nuutsUg: "123",
